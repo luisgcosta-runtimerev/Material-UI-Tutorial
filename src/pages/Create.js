993aa2e15a35bb1453bc83a65c60a37a64f9b35b
Runtime <runtime@Runtimes-MBP.Home>
2022-04-20 11:32:32 +0100

    Buttons & Icons & makeStyles Hook

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,17 +1,51 @@
 import React from 'react'
 import { Typography } from '@mui/material'
+import { Button } from '@mui/material'
+import { Container } from '@mui/material'
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover':{
+      backgroundColor: 'blue'
+    }
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20
+  }
+
+})
 
 export default function Create() {
+  const classes = useStyles()
+
   return (
-    <div>
-      <Typography
-        variant="h6"
-        color="textSecondary"
-        component="h2"
-        gutterBottom
-      >
-        Create a New Note
-      </Typography>
-    </div>
+      <Container>
+        <Typography
+          className={classes.title}
+          variant="h6"
+          color="textSecondary"
+          component="h2"
+          gutterBottom
+        >
+          Create a New Note
+        </Typography>
+
+        <Button
+          className={classes.btn}
+          onClick={() => console.log('you clicked me')}
+          type="submit"
+          color="secondary"
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+
+      </Container>
   )
 }
