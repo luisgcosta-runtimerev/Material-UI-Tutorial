4a66b6264cabc081add3d0a2f65196f10c67a442
Runtime <runtime@Runtimes-MBP.Home>
2022-04-20 10:13:51 +0100

    Material UI - Typography

---
+++ src/pages/Create.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+export default function Create() {
+  return (
+    <div>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+    </div>
+  )
+}
