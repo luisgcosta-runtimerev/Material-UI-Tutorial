347266faa937dcfedd9c566ff02203a8e635d7c7
Luis Costa <runtime@Runtimes-MBP.Home>
2022-04-21 10:17:42 +0100

    Permanent Drawer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import PropType from 'prop-types';
 import { makeStyles } from '@mui/styles';
+import { Drawer, Typography } from '@mui/material';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+  },
   page: {
     background: '#f9f9f9',
     width: '100%',
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
   },
 });
 
@@ -13,10 +25,22 @@ export default function Layout({ children }) {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
       {/* app navbar */}
 
       {/* side drawer */}
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant="h5">
+            Luis Costa
+          </Typography>
+        </div>
+      </Drawer>
 
       {/* app body */}
       <div className={classes.page}>
